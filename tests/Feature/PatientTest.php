718f84c6469edece46e0a71c7b716b6f19d2bb57
Akande Toyyib <akandetoyyib@yahoo.com>
2021-09-23 11:54:59 +0100

    Seeding

--- tests/Feature/PatientTest.php
+++ tests/Feature/PatientTest.php
@@ -29,30 +29,6 @@ class PatientTest extends TestCase
         
         //He should be able to read the patient name
         $response->assertSee($patient->name);
-    }
-
-    /** @test */
-    public function authenticated_users_can_create_a_new_patient()
-    {
-        //Given we have an authenticated user
-        $this->actingAs(User::factory()->create());
-        //And a user object
-        $patient = Patients::factory()->make();
-        //When user submits post request to create patient endpoint
-        $this->post('/patients/store',$patient->toArray());
-        //It gets stored in the database
-        $this->assertEquals(1,Patients::all()->count());
-    }
-
-    /** @test */
-    public function unauthenticated_users_cannot_create_a_new_patient()
-    {
-        //Given we have a user object
-        $patient = Patients::factory()->make();
-        //When unauthenticated user submits post request to create patient endpoint
-        // He should be redirected to login page
-        $this->post('/patients/store',$patient->toArray())
-             ->assertRedirect('/login');
-    }
+    } 
  
 }
